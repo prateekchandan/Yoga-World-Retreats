8bfd939b38ceba48cc933c59e5aa96990f61f4d0
Prateek Chandan <prateekchandan5545@gmail.com>
2016-03-04 04:20:11 +0530

    Added mailer

--- config/mail.php
+++ config/mail.php
@@ -54,7 +54,7 @@ return [
     |
     */
 
-    'from' => ['address' => null, 'name' => null],
+    'from' => ['address' => "support@yogaworldretreats.com", 'name' => "Yoga World Retreats"],
 
     /*
     |--------------------------------------------------------------------------
